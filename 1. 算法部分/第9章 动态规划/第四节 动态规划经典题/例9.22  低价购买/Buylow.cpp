c35e49bd099e63b8ee92d8ca7e20e1e25deb886b
WangYuyang <1109790800@qq.com>
2017-01-16 21:46:16 +0800

    代码提交信息

---
+++ 1. 算法部分/第9章 动态规划/第四节 动态规划经典题/例9.22  低价购买/Buylow.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main()
+{
+	int i,j,k,n,max,num,a[5002],b[5002],f[5002];
+	bool ok[50002];
+	cin>>n;
+	for (i=1;i<=n;i++) cin>>a[i];
+	memset(b,0,sizeof(b));
+	memset(f,0,sizeof(f));
+	b[1]=1;
+	f[1]=1;
+	for (i=2;i<=n+1;i++)
+	{
+		max=0;
+		f[i]=1;
+		for (j=i-1;j>=1;j--)
+		if (a[i]<a[j])
+		  if (b[j]>max)
+		  {
+				max=b[j];
+				memset(ok,1,sizeof(ok));
+				ok[a[j]]=false;
+				f[i]=f[j];
+				}
+		  else if (b[j]==max && ok[a[j]])
+		  {
+				ok[a[j]]=false;
+				f[i]+=f[j];
+				}
+		  b[i]=max+1;
+		} 
+	cout<<b[n+1]-1<<' '<<f[n+1]<<endl;
+	return 0;
+}
